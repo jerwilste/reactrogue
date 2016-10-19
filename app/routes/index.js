05662af9a7ec0cc74b96ded952ca1801686fe94e
Jeremy <jeremywstewart@outlook.com>
2016-10-19 04:18:13 +0000

    problem solved..ish

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,6 +4,6 @@ module.exports = function (app) {
 
     app.route('/')
 		.get(function (req, res) {
-		    res.sendFile(path + '/test.html');
+		    res.sendFile(path + '/public/index.html');
 		});
 }
--- server.js
+++ server.js
@@ -18,6 +18,9 @@ require('dotenv').load();
 
 mongoose.connect(process.env.MONGO_URI);
 
+app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+app.use('/public', express.static(process.cwd() + '/public'));
+
 routes(app);
 
 var port = process.env.PORT || 8080;

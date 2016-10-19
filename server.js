a328c037679e9463902245450d98b4dff9331183
Jeremy <jeremywstewart@outlook.com>
2016-10-19 04:05:17 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+'use strict';
+
+
+var express = require('express');
+var routes = require('./app/routes/index.js');
+var mongoose = require('mongoose');
+//var passport = require('passport');
+//var session = require('express-session');
+var app = express();
+
+var bodyparser = require('body-parser');
+app.use(bodyparser.json() );       // to support JSON-encoded bodies
+app.use(bodyparser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+require('dotenv').load();
+
+mongoose.connect(process.env.MONGO_URI);
+
+routes(app);
+
+var port = process.env.PORT || 8080;
+app.listen(port,  function () {
+	console.log('listening on port ' + port + '...');
+});
